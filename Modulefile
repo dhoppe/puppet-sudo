6502a3f44369486ff82851a64afb3e33aad6a1f4
Dennis Hoppe <dennis.hoppe@credativ.de>
2012-01-20 10:37:44 +0100

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name 'dhoppe-sudo'
+version '0.0.1'
+
+author 'Dennis Hoppe'
+license 'GPLv2'
+project_page 'http://github.com/dhoppe/puppet-sudo'
+source 'git://github.com/dhoppe/puppet-sudo'
+summary 'Sudo Module'
+description 'Sudo Module for Debian, Ubuntu'
---
+++ manifests/init.pp
@@ -0,0 +1,50 @@
+class sudo {
+	if $::lsbdistcodename == "lenny" {
+		file { "/etc/sudoers":
+			owner   => root,
+			group   => root,
+			mode    => 0440,
+			source  => [
+				"puppet:///modules/sudo/$::lsbdistcodename/$::hostname/etc/sudoers",
+				"puppet:///modules/sudo/$::lsbdistcodename/common/etc/sudoers"
+			],
+			require => Package["sudo"],
+		}
+	} else {
+		file { "/etc/sudoers":
+			owner   => root,
+			group   => root,
+			mode    => 0440,
+			source  => "puppet:///modules/sudo/$::lsbdistcodename/etc/sudoers",
+			require => Package["sudo"],
+		}
+
+		file { "/etc/sudoers.d":
+			force   => true,
+			purge   => true,
+			recurse => true,
+			owner   => root,
+			group   => root,
+			mode    => 0440,
+			require => Package["sudo"],
+		}
+	}
+
+	file { "/usr/bin/sudo":
+		owner   => root,
+		group   => root,
+		mode    => 4755,
+	}
+
+	file { "/usr/sbin/visudo":
+		owner   => root,
+		group   => root,
+		mode    => 0755,
+	}
+
+	package { "sudo":
+		ensure => present,
+	}
+}
+
+# vim: tabstop=3
