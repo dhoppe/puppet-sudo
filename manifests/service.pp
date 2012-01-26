63e8ec1242b1c323d2c8137d53d73e792a07ad55
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-01-26 06:52:47 +0100

    Add support for icinga, nagios

---
+++ manifests/service.pp
@@ -0,0 +1,11 @@
+define sudo::service() {
+	file { "/etc/sudoers.d/${name}":
+		owner   => root,
+		group   => root,
+		mode    => 0440,
+		source  => "puppet:///modules/sudo/common/etc/sudoers.d/${name}",
+		require => Package["sudo"],
+	}
+}
+
+# vim: tabstop=3
