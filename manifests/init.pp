83b44586e978a3261ff731b47e3e5b792af88a98
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-05-09 12:04:53 +0200

    Fix several issues detected by puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,50 +1,48 @@
 class sudo {
-	if $::lsbdistcodename == 'lenny' {
-		file { '/etc/sudoers':
-			owner   => root,
-			group   => root,
-			mode    => '0440',
-			source  => [
-				"puppet:///modules/sudo/${::lsbdistcodename}/${::hostname}/etc/sudoers",
-				"puppet:///modules/sudo/${::lsbdistcodename}/common/etc/sudoers"
-			],
-			require => Package['sudo'],
-		}
-	} else {
-		file { '/etc/sudoers':
-			owner   => root,
-			group   => root,
-			mode    => '0440',
-			source  => "puppet:///modules/sudo/${::lsbdistcodename}/etc/sudoers",
-			require => Package['sudo'],
-		}
+  if $::lsbdistcodename == 'lenny' {
+    file { '/etc/sudoers':
+      owner   => root,
+      group   => root,
+      mode    => '0440',
+      source  => [
+        "puppet:///modules/sudo/${::lsbdistcodename}/${::hostname}/etc/sudoers",
+        "puppet:///modules/sudo/${::lsbdistcodename}/common/etc/sudoers"
+      ],
+      require => Package['sudo'],
+    }
+  } else {
+    file { '/etc/sudoers':
+      owner   => root,
+      group   => root,
+      mode    => '0440',
+      source  => "puppet:///modules/sudo/${::lsbdistcodename}/etc/sudoers",
+      require => Package['sudo'],
+    }
 
-		file { '/etc/sudoers.d':
-			force   => true,
-			purge   => true,
-			recurse => true,
-			owner   => root,
-			group   => root,
-			mode    => '0440',
-			require => Package['sudo'],
-		}
-	}
+    file { '/etc/sudoers.d':
+      force   => true,
+      purge   => true,
+      recurse => true,
+      owner   => root,
+      group   => root,
+      mode    => '0440',
+      require => Package['sudo'],
+    }
+  }
 
-	file { '/usr/bin/sudo':
-		owner   => root,
-		group   => root,
-		mode    => '4755',
-	}
+  file { '/usr/bin/sudo':
+    owner   => root,
+    group   => root,
+    mode    => '4755',
+  }
 
-	file { '/usr/sbin/visudo':
-		owner   => root,
-		group   => root,
-		mode    => '0755',
-	}
+  file { '/usr/sbin/visudo':
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+  }
 
-	package { 'sudo':
-		ensure => present,
-	}
+  package { 'sudo':
+    ensure => present,
+  }
 }
-
-# vim: tabstop=3
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,11 +1,9 @@
 define sudo::service() {
-	file { "/etc/sudoers.d/${name}":
-		owner   => root,
-		group   => root,
-		mode    => '0440',
-		source  => "puppet:///modules/sudo/common/etc/sudoers.d/${name}",
-		require => Package['sudo'],
-	}
+  file { "/etc/sudoers.d/${name}":
+    owner   => root,
+    group   => root,
+    mode    => '0440',
+    source  => "puppet:///modules/sudo/common/etc/sudoers.d/${name}",
+    require => Package['sudo'],
+  }
 }
-
-# vim: tabstop=3
