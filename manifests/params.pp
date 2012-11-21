1caa5fdd809884d7a5b422fafc83fc167e8c60f1
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-11-21 16:59:19 +0100

    Remove support for Maverick    - End of Life on 2012-04-12

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class sudo::params {
   case $::lsbdistcodename {
-    'squeeze', 'maverick', 'natty': {
+    'squeeze', 'natty': {
       $admins  = hiera('admin')
       $content = template("sudo/${::lsbdistcodename}/etc/sudoers.erb")
       $source  = undef
